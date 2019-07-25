e8e390970a06fc8ccdde9d582c28bcecf42e24dd
Brooke Kullberg and Reese Lee <brooke@>
2019-07-25 12:03:47 -0700

    update Controller with GetCategories

--- to-do-list/Controllers/SampleDataController.cs
+++ to-do-list/Controllers/SampleDataController.cs
@@ -2,43 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; 
+using to_do_list.Context;
+
 
 namespace to_do_list.Controllers
 {
     [Route("api/[controller]")]
-    public class SampleDataController : Controller
+    public class DataController : Controller
     {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts(int startDateIndex)
+        [HttpGet("[action")]
+        public List<Category> GetCategories(int startDateIndex)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index + startDateIndex).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
-        }
-
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
+            List<Category> category = ToDoListData.GetCategory();
+            return category;
         }
     }
 }
